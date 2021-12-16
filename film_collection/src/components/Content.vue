0267358f9f6b5dcad7b21aae3dc4877b037f3188
Vladimir35-Sashko <sashko852909@gmail.com>
2021-12-16 12:03:47 +0200

    change

---
+++ film_collection/src/components/Content.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content">
+<h1>wfwfewf</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Content extends Vue {
+  // @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
